9ac0bddde3da1012509d60483e3b541aa845c049
Marieke <mariekewpas@gmail.com>
2025-03-31 12:51:45 +0200

    Basic layout setup RecipeList Component

--- recipe-app-front-end/src/app/components/RecipeList/RecipeList.js
+++ recipe-app-front-end/src/app/components/RecipeList/RecipeList.js
@@ -1,9 +1,23 @@
 export default function RecipeList() {
     return (
-        <div>
+        <div className="recipe-list-container">
+            <div className="recipe-title-column">
             <h2>Recipe</h2>
             <h2>Recipe2</h2>
             <h2>Recipe 3</h2>
+            </div>
+            
+            <div className="recipe-calorie-column">
+                <p>800</p>
+                <p>905</p>
+                <p>1202</p>
+            </div>
+
+            <div className="recipe-cuisine-column">
+                <p>Italiaans</p>
+                <p>Japans</p>
+                <p>Midden-Oosters</p>
+            </div>
         </div>
     )
 }
