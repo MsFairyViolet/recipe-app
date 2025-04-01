27b61ef91b694aac95d4f325024df0de45d02c35
Marieke <mariekewpas@gmail.com>
2025-04-01 10:57:04 +0200

    Create bigger mockfile in sync with database values

--- recipe-app-front-end/src/app/components/RecipeList/RecipeList.js
+++ recipe-app-front-end/src/app/components/RecipeList/RecipeList.js
@@ -5,7 +5,7 @@ export default function RecipeList({ recipes }) {
             {recipes.map((recipe) => (
                 <div className="recipe-row" key={recipe.id}>
                     <div className="recipe-title-column">{recipe.name}</div>
-                    <div className="recipe-calorie-column">{recipe.calories}</div>
+                    <div className="recipe-calorie-column">{recipe.servingCalories}</div>
                     <div className="recipe-cuisine-column">{recipe.cuisine}</div>
                 </div>
             ))
