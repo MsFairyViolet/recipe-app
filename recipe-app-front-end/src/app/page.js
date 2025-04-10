6249170d0de0766038d92dc8b860e93be289066a
Marieke <mariekewpas@gmail.com>
2025-04-10 14:02:49 +0200

    Redirect mainpage to /recipe

--- recipe-app-front-end/src/app/page.js
+++ recipe-app-front-end/src/app/page.js
@@ -1,32 +1,5 @@
-// "use client"
+import { redirect } from 'next/navigation';
 
-// import {useState, useEffect} from "react"
-// import recipesData from "../../data/mockrecipes.json"
-// import RecipeListPage from "../components/RecipeList/RecipeListPage";
-// import ViewRecipe from "../components/ViewRecipe/ViewRecipe";
-
-
-// export default function RecipeApp() {
-//   const [recipes, setRecipes] = useState([])
-
-//   useEffect(() => {
-//   setRecipes(recipesData)
-//   }, [])
-
-//   useEffect(() => {
-//     console.log(recipes[0])
-//   })
-
-//   return (
-//     <div className="app">
-//       <main>
-//         <RecipeListPage recipes={recipes}/>
-        
-//         {recipes.length > 0 ?
-//         <ViewRecipe recipe={recipes[0]}/> : <p className="no-results-message">Loading recipe...</p>
-//         }
-        
-//       </main>
-//     </div>
-//   );
-// }
+export default function Home() {
+  redirect('/recipe');
+}
