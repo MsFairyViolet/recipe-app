b7db4c35895e9aaf1716c17990acb84564e30281
Marieke <mariekewpas@gmail.com>
2025-04-10 15:06:50 +0200

    Replace mockdata json by API GET fetch

--- recipe-app-front-end/src/app/recipe/page.js
+++ recipe-app-front-end/src/app/recipe/page.js
@@ -6,11 +6,36 @@ import RecipeListPage from "@components/RecipeList/RecipeListPage";
 
 export default function RecipeListContainer() {
     const [recipes, setRecipes] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    // useEffect(() => {
+    //     setRecipes(recipesData)
+    // }, [])
+
+    const fetchRecipes = () => {
+        fetch("api/recipe")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to fetch recipes")
+            }
+            return response.json()
+        })
+        .then((data) => {
+            setRecipes(data)
+            setLoading(false)
+        })
+        .catch((error) => {
+            console.log("Error fetching recipes: ", error)
+            setError(error.message)
+            setLoading(false)
+        })
+    }
 
     useEffect(() => {
-        setRecipes(recipesData)
+        fetchRecipes()
     }, [])
-
+    
     // useEffect(() => {
     //     console.log(recipes[0])
     // })
