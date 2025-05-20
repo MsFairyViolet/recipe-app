ac6b125904e139d8d5522b6c22036e2f77d54f6f
Robin Spek <robinspek@gmail.com>
2025-05-20 13:57:23 +0200

    Add docker image creation
    We build one image that has two services managed by pm2 node package,
    the frontend and backend have their own service. SSL terminating &
    routing calls to the correct service is handled by nginx in front.
    
    Notes:
    
    - We changed to have application-{local/productin}.properties in the
      repo so that we have distinct resetting behavior and credentials. This
      will break current environments which needs resolving.
    - There is now a convenience script to start the dev with the correct
      configuration.

--- recipe-app-back-end/src/main/kotlin/nl/rmspek/recipes/RecipesApplication.kt
+++ recipe-app-back-end/src/main/kotlin/nl/rmspek/recipes/RecipesApplication.kt
@@ -2,16 +2,9 @@ package nl.rmspek.recipes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class RecipesApplication: WebMvcConfigurer {
-    val frontendRoutes = listOf("", "about")
-    override fun addViewControllers(registry: ViewControllerRegistry) {
-        frontendRoutes.forEach { registry.addViewController("/$it").setViewName("index.html") }
-    }
-}
+class RecipesApplication
 
 fun main(args: Array<String>) {
     runApplication<RecipesApplication>(*args)
