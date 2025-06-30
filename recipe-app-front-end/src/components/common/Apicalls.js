cfd66873c487954a3b3e884699e48daeff34b869
Marieke <mariekewpas@gmail.com>
2025-06-30 15:15:39 +0200

    Seperate API calls from pages for resusability. Delete notes-file.

--- recipe-app-front-end/src/components/common/Apicalls.js
+++ recipe-app-front-end/src/components/common/Apicalls.js
@@ -49,21 +49,21 @@ export async function getAmountTypes() {
 // Edit Recipe /recipe/id/edit/page.js/EditRecipe.js/EditRecipeIngredientList.js
 // /ingredients/page.js/IngredientsPage.js
 export async function addIngredient(name) {
-   const res = await fetch('/api/ingredient', {
+   const response = await fetch('/api/ingredient', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name }),
    })
 
-   if (!res.ok) {
+   if (!response.ok) {
       throw new Error('Failed to add ingredient')
    }
-   return res.json()
+   return response.json()
 }
 
 // Edit Recipe /recipe/id/edit/page.js/EditRecipe.js
 export async function createRecipe(recipe) {
-   fetch("/api/recipe", {
+   const response = await fetch("/api/recipe", {
       method: "POST",
       headers: {
          "Content-Type": "application/json"
@@ -71,8 +71,59 @@ export async function createRecipe(recipe) {
       body: JSON.stringify(recipe)
    })
    if (!response.ok) {
-      alert("Failed to create recipe.")
       throw new Error("Failed to create recipe")
    }
    return response.json()
+}
+
+// /ingredients/page.js/IngredientsPage.js
+export async function patchIngredient(id, updates) {
+   const response = await fetch(`api/ingredient/${id}`, {
+      method: "PATCH",
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updates)
+   })
+   if (!response.ok) {
+      throw new Error("Failed to update ingredient.");
+   }
+   return response.json()
+}
+
+// Edit Recipe /recipe/id/edit/page.js/EditRecipe.js
+export async function updateRecipe(recipe, newRecipe) {
+   const response = await fetch(`/api/recipe/${recipe.id}`, {
+      method: "PATCH",
+      headers: {
+         "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newRecipe)
+   })
+   if (!response.ok) {
+      throw new Error("Failed to update recipe.")
+   }
+   return response.json()
+}
+
+// /ingredients/page.js/IngredientsPage.js
+export async function deleteIngredient(id) {
+   const response = await fetch(`/api/ingredient/${id}`, {
+      method: "DELETE",
+   })
+
+   if (!response.ok) {
+      throw new Error("Failed to delete ingredient.")
+   }
+}
+
+// Edit Recipe /recipe/id/edit/page.js/EditRecipe.js
+export async function deleteRecipe(id) {
+   const response = await fetch(`/api/recipe/${id}`, {
+      method: "DELETE",
+   })
+
+   if (!response.ok) {
+      throw new Error("Failed to delete recipe")
+   }
 }
