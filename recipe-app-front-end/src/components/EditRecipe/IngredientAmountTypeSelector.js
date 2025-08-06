d7471d71ea2ed7a440de130cc95dc8157d37bc03
Marieke <mariekewpas@gmail.com>
2025-08-06 12:00:33 +0200

    Removed default select that was used for reference

--- recipe-app-front-end/src/components/EditRecipe/IngredientAmountTypeSelector.js
+++ recipe-app-front-end/src/components/EditRecipe/IngredientAmountTypeSelector.js
@@ -33,12 +33,6 @@ export default function IngredientAmountTypeSelector({ ingredient, row, amountTy
                     </ul>
                 )}
             </div>
-            {/* Default select element for reference */}
-            <select data-test="amount-type" className="third-column ingredient-input" type="text" value={ingredient.amountType} onChange={(e) => handleIngredientChange(index, "amountType", e.target.value)}>
-                {amountTypes.map((type) => (
-                    <option key={type.amountType} value={type.amountType}>{type.amountType}</option>
-                ))}
-            </select>
         </>
     )
 }
