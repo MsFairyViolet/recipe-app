1c478c3efd5ad937ac7b4d402780282c32b14a53
Marieke <mariekewpas@gmail.com>
2025-08-12 12:52:48 +0200

    Changed favicon and location

--- recipe-app-front-end/src/app/layout.js
+++ recipe-app-front-end/src/app/layout.js
@@ -11,6 +11,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+      </head>
       <body>
         <NavBar />
         <div className="page-container">
