aa8a318e9e258a29de98ee7cab2d1bf0f6383b2b
MsFairyViolet <168975189+MsFairyViolet@users.noreply.github.com>
2025-08-26 13:27:51 +0200

    Apple Fav Icon specifications

--- recipe-app-front-end/src/app/layout.js
+++ recipe-app-front-end/src/app/layout.js
@@ -11,7 +11,10 @@ export const metadata = {
       { url: "/icons/favicon-16x16.png", type: "image/png" },
       { url: "/icons/favicon-32x32.png", type: "image/png" }
     ],
-    apple: "/icons/apple-touch-icon.png",
+    apple: [
+      { url: "/icons/apple-touch-icon.png", sizes: "180x180", type: "image/png"},
+      { url: "/icons/apple-touch-icon.png"}
+    ],
     shortcut: "/icons/favicon.ico",
   },
   manifest: "/icons/site.webmanifest"
