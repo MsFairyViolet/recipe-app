f88990eb71cb026793a740ca1522acea6d308a01
MsFairyViolet <168975189+MsFairyViolet@users.noreply.github.com>
2025-10-14 11:47:37 +0200

    Styling Search Clear Button

--- recipe-app-front-end/src/components/common/SearchBar.js
+++ recipe-app-front-end/src/components/common/SearchBar.js
@@ -1,8 +1,8 @@
 export default function SearchBar({value, changeValue, placeholder}) {
     return (
-        <>
+        <div className="search-container">
         <input data-test="search-bar" className="search-bar" value={value} placeholder={placeholder} onChange={(e) => changeValue(e.target.value)}></input>
-        <button data-test="search-bar-clear" className="clear-button" onClick={() => changeValue("")}>╳</button>
-        </>
+        <button data-test="search-bar-clear" className="clear-button" onClick={() => changeValue("")}>✕</button>
+        </div>
     )
 }
